97f9e91585a70a8fafc48f9403162c910e3bbdff
DomeniGeco <domenigeco@gmail.com>
2026-05-20 14:21:18 +0200

    Enhance INSTALL.bat to support version installation
    Added functionality to install a specific version of NoOpenBin.

--- INSTALL.bat
+++ INSTALL.bat
@@ -5,6 +5,7 @@
 :: - Nuova versione
 :: - Manutenzione
 :: - Contribuire
+:: - Nuove funzioni
 
 
 :: MAIN CLI
@@ -21,6 +22,9 @@ echo.
 powershell -Command "Write-Host '- nim install' -ForegroundColor Yellow"
 echo    Installa
 
+powershell -Command "Write-Host '- nim iv <versione>' -ForegroundColor Yellow"
+echo    Installa una versione specifica
+
 powershell -Command "Write-Host '- nim escape' -ForegroundColor Yellow"
 echo    Esci
 
@@ -28,6 +32,7 @@ echo.
 set /p "PROMPT=>>"
 
 if "%PROMPT%"=="nim install" goto install_user
+if "%PROMPT:~0,7%"=="nim iv " goto iv
 if "%PROMPT%"=="nim escape" exit /b
 
 goto main
@@ -82,3 +87,53 @@ echo Riavvia il terminale per usare il comando.
 pause
 goto main
 
+:: VERSIONE SPECIFICA
+
+:iv
+set "VERSION=%PROMPT:~7%"
+set "INSTDIR=%LOCALAPPDATA%\NoOpenBin"
+set "INSTURL=https://github.com/DeMENIGECO/NoOpenBin/raw/refs/heads/main/bin/nob_v%VERSION%_windows-64.zip"
+
+echo.
+echo Installazione NoOpenBin %VERSION% per questo utente...
+
+:: Cache temporanea
+set "TMPZIP=%TEMP%\nob_install.zip"
+
+echo Download archivio...
+powershell -NoProfile -ExecutionPolicy Bypass -Command ^
+"$ProgressPreference='SilentlyContinue'; Invoke-WebRequest -Uri '%INSTURL%' -OutFile '%TMPZIP%'"
+
+if errorlevel 1 (
+    echo Errore durante il download.
+    pause
+    goto main
+)
+
+echo Creazione cartella...
+if not exist "%INSTDIR%" mkdir "%INSTDIR%"
+
+echo Estrazione archivio...
+powershell -NoProfile -ExecutionPolicy Bypass -Command ^
+"Expand-Archive -Path '%TMPZIP%' -DestinationPath '%INSTDIR%' -Force"
+
+if errorlevel 1 (
+    echo Errore durante l'estrazione.
+    pause
+    goto main
+)
+
+echo Configurazione PATH...
+
+powershell -NoProfile -ExecutionPolicy Bypass -Command "$path=[Environment]::GetEnvironmentVariable('Path','User'); if($path -notlike '*%INSTDIR%*'){ [Environment]::SetEnvironmentVariable('Path',$path + ';%INSTDIR%','User') }"
+
+:: Aggiorna PATH della sessione corrente
+set "PATH=%PATH%;%INSTDIR%"
+
+del "%TMPZIP%" >nul 2>&1
+
+echo.
+echo Installazione completata!
+echo Riavvia il terminale per usare il comando.
+pause
+goto main
